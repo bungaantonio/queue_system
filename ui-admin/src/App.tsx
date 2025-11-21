b3230e1983d31c57fbcb88f3ad376466ccf0e63a
Bunga António <bunga.antonio@osl-itservice.com>
2025-11-21 15:11:47 +0100

    feat: restructure queue management with new data provider, implement QueueList component, and remove legacy auth and data providers

--- ui-admin/src/App.tsx
+++ ui-admin/src/App.tsx
@@ -1,12 +1,14 @@
 import { Admin, Resource } from "react-admin";
-import operatorDataProvider from "./dataProvider";
-import { authProvider } from "./authProvider";
+import operatorDataProvider from "./operators/dataProvider";
+import { authProvider } from "./operators/authProvider";
 import { OperatorsList } from "./operators/OperatorsList";
 import { OperatorsCreate } from "./operators/OperatorsCreate";
 import { OperatorsEdit } from "./operators/OperatorsEdit";
+import { QueueList } from "./dashboard/components/QueueList";
 
 export const App = () => (
     <Admin authProvider={authProvider} dataProvider={operatorDataProvider}>
+        <Resource name="queue" list={QueueList} />
         <Resource
             name="operators"
             list={OperatorsList}
