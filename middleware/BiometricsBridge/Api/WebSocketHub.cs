1cb2af8f76f8af461135eb36562d7075a10a742d
Bunga António <bunga.antonio@hotmail.com>
2025-12-01 05:33:32 +0100

    Initial middleware project

---
+++ middleware/BiometricsBridge/Api/WebSocketHub.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace BiometricsBridge.Api
+{
+    public class WebSocketHub : Hub
+    {
+        public async Task SendProgress(string message)
+        {
+            await Clients.All.SendAsync("progress", message);
+        }
+
+        public async Task SendCaptureResult(string hash, int fingerIndex)
+        {
+            await Clients.All.SendAsync("captureComplete", new { hash, fingerIndex });
+        }
+    }
+}
--- middleware/BiometricsBridge/Program.cs
+++ middleware/BiometricsBridge/Program.cs
@@ -1,7 +1,27 @@
 using BiometricsBridge;
+using BiometricsBridge.Api;
+using BiometricsBridge.Services;
 
-var builder = Host.CreateApplicationBuilder(args);
+var builder = WebApplication.CreateBuilder(args);
+
+// Logging
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
+
+// Serviços
+builder.Services.AddSingleton<DeviceService>();
+builder.Services.AddSingleton<CaptureService>();
 builder.Services.AddHostedService<Worker>();
 
-var host = builder.Build();
-host.Run();
+builder.Services.AddControllers();
+builder.Services.AddSignalR();
+
+var app = builder.Build();
+
+app.UseRouting();
+
+// Substitua UseEndpoints por registros de rotas de nível superior
+app.MapControllers();
+app.MapHub<WebSocketHub>("/api/v1/stream");
+
+app.Run();
